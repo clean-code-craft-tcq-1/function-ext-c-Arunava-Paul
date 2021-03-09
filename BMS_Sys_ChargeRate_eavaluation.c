6c546cd75b2d9f41b8e46525a2fe802c4d1180af
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-10 01:10:22 +0530

    Update BMS_Sys_ChargeRate_eavaluation.c

--- BMS_Sys_ChargeRate_eavaluation.c
+++ BMS_Sys_ChargeRate_eavaluation.c
@@ -12,6 +12,40 @@
 extern Sysfault_type_en Sysfault_global;
 extern Fault_type_en Fault_type_global;
 
+
+/**********************
+
+struct SensorReadValue_eval_st
+{
+	float low_value;
+	float high_value;
+	Fault_type_en f_type;
+	Sysfault_type_en sf_type;	
+};
+
+*************************/
+struct SensorReadValue_eval_st chargerate_eval[MAX_EVALUATION_TYPES] =					
+{																	\
+	{																\
+		MIN_CHARGE_RATE,											\
+		MIN_SYSTEM_STOP_CHARGE_RATE,								\
+		Fault_chargeRate_low,										\
+		Sysfault_Sleep,												\
+	},																\
+	{																\
+		MIN_ALLOWED_CHARGE_RATE,									\
+		MAX_ALLOWED_CHARGE_RATE,									\
+		Fault_none,													\
+		Sysfault_none,												\
+	},																\
+	{																\
+		MAX_SYSTEM_STOP_CHARGE_RATE,								\
+		MAX_BREACH_CHARGE_RATE,										\
+		Fault_chargeRate_high,										\
+		Sysfault_disconnect_charger,								\
+	}																\
+};
+
 void Fc_Charge_Rate_Sensor_check(void)
 {
 	int Retval;
@@ -27,19 +61,19 @@ void Fc_Charge_Rate_Sensor_check(void)
 	}
 	else
 	{
-		if(C_rate < MIN_CHARGE_RATE)
-		{
-			Fault_type_global = Fault_chargeRate_low;
-			Sysfault_global = Sysfault_none;
-		}
-		else if(C_rate > MAX_CHARGE_RATE)
-		{
-			Fault_type_global = Fault_chargeRate_high;
-			Sysfault_global = Sysfault_disconnect_charger;
-		}
-		else
-		{
-			//system normal,do nothing
-		}
+		Fc_ChargeRate_Evaluation(C_rate);
 	}
 }/*end of Fc_Charge_Rate_Sensor_check*/
+
+void Fc_ChargeRate_Evaluation(float measured_chrate)
+{	
+	int idx = 0;
+	for(idx = 0 ; idx<MAX_EVALUATION_TYPES ; idx++ )
+		{
+			if(isInRange(chargerate_eval[idx].low_value , chargerate_eval[idx].high_value , measured_chrate ))
+			{
+				Sysfault_global = chargerate_eval[idx].sf_type;
+				Fault_type_global = chargerate_eval[idx].f_type;
+			}
+		}
+}/*end of Fc_ChargeRate_Evaluation*/
